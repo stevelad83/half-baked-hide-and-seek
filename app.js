497accefe07790d1343dbf06b2fc40612010caa6
Steven Bryant <stevelad_83@yahoo.com>
2022-10-31 15:05:47 -0400

    adding function to reset state

--- app.js
+++ app.js
@@ -36,10 +36,20 @@ boulderButton.addEventListener('click', () => {
 });
 
 function handleGuess(correctSpot, userGuess) {
-    // reset the styles
-    // then increment the guesses
+    resetFaces();
+    totalGuesses++;
+    if (userGuess === correctSpot) {
+        correctGuesses++;
+        console.log('correct');
+    }
     // then grab the appropriate container element for the correct guess from the DOM
     // then add the face class to that element so that the face shows up
     // then if the user guess is correct, increment the correct guesses
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
+}
+
+function resetFaces() {
+    shedContainer.classList.remove('face');
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
 }
